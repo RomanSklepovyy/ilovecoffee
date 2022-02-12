ba8eb28e92b2337ea72918804fa601a47dff3694
RomanSklepovyy <48484690+RomanSklepovyy@users.noreply.github.com>
2022-02-12 22:12:47 +0200

    add custom Protocol decorator

---
+++ src/common/decorators/protocol.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Protocol = createParamDecorator(
+  (defaultValue: string, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.protocol;
+  },
+);
